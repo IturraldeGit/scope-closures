6b14b84ec73801affe8ee4c0cb8780e29e19b6cf
Daniel Iturralde <daniel_iturralde_b@hotmail.com>
2023-09-27 21:53:14 -0500

    Redeclare and Reassign

---
+++ src/scope/re.js
@@ -0,0 +1,32 @@
+// Var
+var firstName; //
+firstName = 'Daniel';
+console.log(firstName);
+
+var lastName = 'David'; // declarar / asignar
+lastName = 'Ana'; // reasignar
+console.log(lastName);
+
+var secondName = 'David'; //  declarando / asignando
+var secondName = 'Ana';  // reasignando
+console.log(secondName);
+
+
+// Let
+let fruit = 'Apple'; // declarar y asignar
+fruit = 'Kiwi'; // reasignar
+// let fruit = 'Banana'; // No se puede redeclarar
+console.log(fruit);
+
+
+// Const
+const animal = 'dog'; // declarar y asignar
+// animal = 'cat'; // No se puede reasignar
+console.log(animal);
+
+const vehicles = []; // declarar y asignar
+vehicles.push('🏎') // método push de los arrays
+console.log(vehicles);
+
+vehicles.pop(); // método pop de los arrays
+console.log(vehicles);
