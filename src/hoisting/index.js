d22e6a51fa789e254917e536c34353db4d0e9fe9
Daniel Iturralde <daniel_iturralde_b@hotmail.com>
2023-10-02 04:26:09 -0500

    Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,15 @@
+nameOfDog; //undefined
+
+console.log(nameOfDog);
+var nameOfDog = 'Elmo'
+console.log(nameOfDog);
+
+
+
+var elmo; // undefined
+nameOfCat();
+
+function nameOfCat() {
+    console.log(`El mejor perrito es ${elmo}`);
+}
+var elmo = 'Elmo'
---
+++ src/hoisting/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+nameOfCat();
+
+function nameOfCat() {
+    console.log(`El mejor perrito es ${elmo}`);
+}
+var elmo = 'Elmo'
